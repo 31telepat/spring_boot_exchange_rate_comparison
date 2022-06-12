e26de83f6f9691215cf493a318d5de98be7f1c38
DESKTOP-TP2AU31\Пользователь <max31.86@yandex.ru>
2022-06-12 18:32:25 +0300

    Initial project

---
+++ src/main/java/com/tsemenouski/spring_boot_exchange_rate_comparison/SpringBootExchangeRateComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.tsemenouski.spring_boot_exchange_rate_comparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExchangeRateComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExchangeRateComparisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsemenouski/spring_boot_exchange_rate_comparison/SpringBootExchangeRateComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsemenouski.spring_boot_exchange_rate_comparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExchangeRateComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
